3116199bfc25f70de8d89f6435c568fcbf91abd9
Jorge Zuniga <jlzuniga001@att.net>
2015-06-08 21:09:19 -0500

    Updates for Wikipedia API URLs

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@ var markers = [];
 
 // Model
 locArray = [
-              {name: "Casino Monte Carlo" 				,lat: 43.739146, long: 7.428073, urlName: "Monte%20Carlo%20Casino"						, markerNum: 0}
+              {name: "Casino Monte Carlo" 				,lat: 43.739146, long: 7.428073, urlName: "Monte_Carlo_Casino"						, markerNum: 0}
              ,{name: "Cathedral of Saint Charles"		,lat: 43.742619, long: 7.427236, urlName: "Cathedral%20of%20Saint%20Charles%20Monaco"	, markerNum: 1} 
              ,{name: "Church of Saint Devote"			,lat: 43.737426, long: 7.421087, urlName: "Sainte-Dévote%20Chapel"						, markerNum: 2}
-             ,{name: "Exotic Gardens of Monaco"			,lat: 43.731381, long: 7.414032, urlName: "Exotic%20Gardens%20of%20Monaco"				, markerNum: 3}
+             ,{name: "Exotic Gardens of Monaco"			,lat: 43.731381, long: 7.414032, urlName: "Jardin_Exotique_de_Monaco"				, markerNum: 3}
              ,{name: "Francois Grimaldi Statue"			,lat: 43.731372, long: 7.421282, urlName: "Fran%C3%A7ois_Grimaldi"						, markerNum: 4}	
-             ,{name: "Grimaldi Forum Monaco"			,lat: 43.744038, long: 7.431400, urlName: "Grimaldi%20Forum%20Monaco"					, markerNum: 5}
+             ,{name: "Grimaldi Forum"					,lat: 43.744038, long: 7.431400, urlName: "Grimaldi_Forum"								, markerNum: 5}
              ,{name: "Hotel Hermitage Monte-Carlo"		,lat: 43.738240, long: 7.425863, urlName: "Hotel%20Hermitage%20Monte-Carlo"				, markerNum: 6}
              ,{name: "Japanese Gardens"					,lat: 43.742343, long: 7.430889, urlName: "Japanese%20Gardens%20Monaco"					, markerNum: 7}
              ,{name: "Monaco Grand Prix"				,lat: 43.733628, long: 7.421658, urlName: "Monaco%20Grand%20Prix"						, markerNum: 8}
              ,{name: "Monte-Carlo Bay Hotel & Resort"	,lat: 43.748983, long: 7.438866, urlName: "Monte-Carlo%20Bay%20Hotel%20&%20Resort"		, markerNum: 9}
              ,{name: "Prince's Palace of Monaco"		,lat: 43.731444, long: 7.419906, urlName: "Prince's%20Palace%20of%20Monaco"				, markerNum: 10}             
              ,{name: "Royal Marine Museum & Aquarium"	,lat: 43.730612, long: 7.425276, urlName: "Royal%20Marine%20Museum%20&%20Aquarium%20Monaco"	, markerNum: 11}             
-             ,{name: "Saint Nicholas Cathedral" 		,lat: 43.730287, long: 7.422677, urlName: "Saint%20Nicholas%20Cathedral%20Monaco" 		, markerNum: 12}            
+             ,{name: "Saint Nicholas Cathedral" 		,lat: 43.730287, long: 7.422677, urlName: "Saint_Nicholas_Cathedral,_Monaco" 		, markerNum: 12}            
            ];
 
 
@@ -38,7 +38,7 @@ var initMap = function(){
               position: new google.maps.LatLng(locArray[i].lat, locArray[i].long)
     		, map: map
     		, title: locArray[i].name  
-    		, wikiLink: '<br><a href='+wikiUrl+'>'+'Wikipedia Article'+'</a>'
+    		, wikiLink: '<br><a href=https://en.wikipedia.org/wiki/'+locArray[i].urlName+'>'+'Wikipedia Article'+'</a>'
         });
 
     	// Google Maps API  on click addListener
@@ -55,26 +55,15 @@ var initMap = function(){
         })(marker));
         markers.push(marker);    
         
-     // Wikipedia AJAX request     
-     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + locArray[i].urlName + '&format=json&callback=wikiCallback'; 
-     /*var wikiRequestTimeout = setTimeout(function(){
+     // Wikipedia JSON request     
+    // var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + locArray[i].urlName + '&format=json&callback=wikiCallback'; 
+    // var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&titles='+ locArray[i].urlName;
+      // var wikiUrl = 'https://en.wikipedia.org/wiki/action=query&list=search&format=json&srsearch='+ locArray[i].urlName;
+       //var wikiUrl = 'https://en.wikipedia.org/wiki/'+locArray[i].urlName;
+       /*var wikiRequestTimeout = setTimeout(function(){
     	 wikiUrl.text("failed to get wikipedia resources");
-          }, 8000); */
-          
-          $.ajax({
-          	 url: wikiUrl
-          	,dataType: "jsonp"
-          	//,jsonp: "callback"
-          	,success: function( response ) {
-          		var articleList = response[1];          		
-          		for (var i = 0; i < articleList.length; i++) {
-          			articleStr = articleList[i];
-          			var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-          		};
-          		//clearTimeout(wikiRequestTimeout);
-          	}
-          });
-          //return false;
+          }, 8000); 
+       return false;*/
   }
 };
 
