bca83b62f5f7636a9977d97232d947a00a5d8128
Jorge Zuniga <jlzuniga001@att.net>
2015-07-25 17:16:30 -0500

    Clean up formatting, add comments

--- js/app.js
+++ js/app.js
@@ -32,21 +32,21 @@ var infoBubble = new InfoBubble({
 infoBubble.addTab('Wikipedia','Oops! Content is not available.');
 infoBubble.addTab('Street View','Oops! Content is not available.');
 
-// MODEL
+// Model
 var places = [
-     { id:  1, name: 'François Grimaldi'			 	,map: map.gMap, position: { lat: 43.731372, lng: 7.421282 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
-    ,{ id:  2, name: 'Grimaldi Forum'			 		,map: map.gMap, position: { lat: 43.744038, lng: 7.431400 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
-    ,{ id:  3, name: 'Hotel Hermitage Monte-Carlo'		,map: map.gMap, position: { lat: 43.738240, lng: 7.425863 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
-    ,{ id:  4, name: 'Jardin Exotique de Monaco'	 	,map: map.gMap, position: { lat: 43.731381, lng: 7.414032 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
-    ,{ id:  5, name: 'Monaco Grand Prix'			 	,map: map.gMap, position: { lat: 43.733628, lng: 7.421658 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
-    ,{ id:  6, name: 'Monte Carlo Casino'		 		,map: map.gMap, position: { lat: 43.739444, lng: 7.428889 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
-    ,{ id:  7, name: 'Monte-Carlo Bay Hotel & Resort'	,map: map.gMap, position: { lat: 43.748983, lng: 7.438866 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
-    ,{ id:  8, name: 'Oceanographic Museum'				,map: map.gMap, position: { lat: 43.730833, lng: 7.425278 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+     { id:  1, name: 'François Grimaldi' ,map: map.gMap, position: { lat: 43.731372, lng: 7.421282 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+    ,{ id:  2, name: 'Grimaldi Forum' ,map: map.gMap, position: { lat: 43.744038, lng: 7.431400 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+    ,{ id:  3, name: 'Hotel Hermitage Monte-Carlo' ,map: map.gMap, position: { lat: 43.738240, lng: 7.425863 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+    ,{ id:  4, name: 'Jardin Exotique de Monaco' ,map: map.gMap, position: { lat: 43.731381, lng: 7.414032 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+    ,{ id:  5, name: 'Monaco Grand Prix' ,map: map.gMap, position: { lat: 43.733628, lng: 7.421658 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+    ,{ id:  6, name: 'Monte Carlo Casino' ,map: map.gMap, position: { lat: 43.739444, lng: 7.428889 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+    ,{ id:  7, name: 'Monte-Carlo Bay Hotel & Resort' ,map: map.gMap, position: { lat: 43.748983, lng: 7.438866 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+    ,{ id:  8, name: 'Oceanographic Museum'	,map: map.gMap, position: { lat: 43.730833, lng: 7.425278 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
     ,{ id:  9, name: 'Saint Nicholas Cathedral, Monaco'	,map: map.gMap, position: { lat: 43.730287, lng: 7.422677 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 } 
-    ,{ id: 10, name: 'Sainte-Dévote Chapel'				,map: map.gMap, position: { lat: 43.737426, lng: 7.421087 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
+    ,{ id: 10, name: 'Sainte-Dévote Chapel' ,map: map.gMap, position: { lat: 43.737426, lng: 7.421087 }, icon: null, animation: google.maps.Animation.DROP, selected: 0 }
 ];
 
-//create marker
+// Marker creation
 var Place = function(place) {
     place.name = ko.observable(place.name);
     place.selected = ko.observable(place.selected);
@@ -57,27 +57,27 @@ var Place = function(place) {
     return marker;
 };
 
-//octopus ViewModel
+// ViewModel
 var ViewModel = function(){
     var self = this;
     this.list = ko.observableArray([]);
 
-    //create and bind our markers from our raw data
+    // Create and bind markers using the "places" array
     places.forEach(function(place){
         var marker = new Place(place);
-        //add event listener using a closure.
+        // Add an event listener using a closure
         google.maps.event.addListener(marker, 'click', (function(Copy) {
             return function() {
                 self.setCurrentPlace(Copy);
             };
         })(marker));
-
         self.list().push(marker);
     });    
     
+    // Ajax call to Wikipedia
     this.wikiCall = function(data){
         var wikiTimeOut = setTimeout(function(){
-            infoBubble.updateTab(0, '<div class="infoBubble">Wiki</div>', "request failed");
+            infoBubble.updateTab(0, '<div class="infoBubble">Wikipedia</div>', "So sorry, the request to Wikipedia has failed");
             infoBubble.updateContent_();
         }, 8000);
         $.ajax({
@@ -101,9 +101,10 @@ var ViewModel = function(){
         });
     };
     
+    // Google Maps Street View
     this.streetView = function(data){
         var img = data.position.A + "," + data.position.F;
-        var contentString = '<img class="bgimg" alt="failed to load image...check internet" src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+img+'">';
+        var contentString = '<img class="bgimg" alt="So sorry, the image failed to load." src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+img+'">';
         infoBubble.updateTab(1,'<div class="infoBubble">Street View</div>',contentString);
         infoBubble.updateContent_();
     };
@@ -116,7 +117,6 @@ var ViewModel = function(){
         data.setIcon(iconSelected);
         data.selected(1);
         self.currentPlace(data);
-        //self.ajaxCallNY(data);
         self.wikiCall(data);
         self.streetView(data);
         infoBubble.open(map.gMap, data);
@@ -124,7 +124,6 @@ var ViewModel = function(){
     };
     this.currentPlace = ko.observable( this.list()[0] );
     this.searchBox = ko.observable("");
-    //use ko utils arrayFilter to filter the array of markers to implement the search functionality
     this.searchPlaces = ko.computed(function() {
             if(self.searchBox() === "") {
                 return self.list();
@@ -134,11 +133,5 @@ var ViewModel = function(){
                 });
             }
         });
-    $( "#placesBtn" ).click(function() {
-        $( "#places" ).toggleClass( "hidden-xs" );
-    });
-    window.onload = function() {
-        self.setCurrentPlace(self.list()[0]);
-    };
 };
 ko.applyBindings(new ViewModel());
